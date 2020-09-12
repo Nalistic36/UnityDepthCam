3f981a2215922ff335063d9d0c18aafcba6cc0e7
unknown <mario.fdez.ortega@gmail.com>
2020-09-12 13:23:17 +0200

    remove player 2

---
+++ TFM/Assets/Scripts/HeadTracking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SimpleKinectManager))]
+public class HeadTracking : MonoBehaviour
+{
+    private SimpleKinectManager _manager;
+    private int _head;
+
+    void Awake()
+    {
+        _manager = GetComponent<SimpleKinectManager>();
+        _head = (int)KinectWrapper.NuiSkeletonPositionIndex.Head;
+    }
+
+    private void Update()
+    {
+        Debug.Log(UpdateHeadPose());
+    }
+
+    private Vector3 UpdateHeadPose()
+    {
+        return _manager.GetJointPosition(_head);
+    }
+}
